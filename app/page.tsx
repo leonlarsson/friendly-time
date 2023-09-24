d1a2ac9893a8c697e0b3b7096ca42b5e6a655306
Leon San José Larsson <leonlarsson8@gmail.com>
2023-09-24 20:27:16 +0200

    add description

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import Input from "./components/Input";
 export default function Home() {
   return (
     <main className="w-full max-w-4xl">
-      <h1 className="mb-4 text-4xl font-black">Friendly Time</h1>
+      <h1 className="text-4xl font-black">Friendly Time</h1>
+      <div className="mb-4">Input your desired date and time through natural language or use the date/time selector.</div>
+
       <Input />
     </main>
   );
