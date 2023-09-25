40931e02fff8142966a0ea4ed847e86f63e1890b
Leon San José Larsson <leonlarsson8@gmail.com>
2023-09-25 12:20:47 +0200

    Settings: use data structure

--- app/components/Settings.tsx
+++ app/components/Settings.tsx
@@ -9,6 +9,27 @@ type Props = {
 };
 
 export default ({ useDateInput, strictMode, setStrictMode, use24HourFormat, setUse24HourFormat, sortTimezonesByTime, setSortTimezonesByTime }: Props) => {
+  const settings = [
+    {
+      name: "Strict mode",
+      description: "If enabled, parse only formal date patterns.",
+      value: strictMode,
+      setValue: setStrictMode
+    },
+    {
+      name: "Display 24-hour time format",
+      description: "If enabled, display time using the 24-hour format for the timezones. Otherwise, use 12-hour format.",
+      value: use24HourFormat,
+      setValue: setUse24HourFormat
+    },
+    {
+      name: "Sort timezones by time",
+      description: "If enabled, sort list of timezones by their UTC offset. Otherwise, sort alphabetically.",
+      value: sortTimezonesByTime,
+      setValue: setSortTimezonesByTime
+    }
+  ];
+
   return (
     <details className="select-none">
       <summary className="mt-4 cursor-pointer text-xl font-semibold">Settings & Info</summary>
@@ -17,32 +38,15 @@ export default ({ useDateInput, strictMode, setStrictMode, use24HourFormat, setU
       </span>
 
       <div className="flex flex-col gap-2 py-2">
-        {/* Strict mode */}
-        <div>
-          <input type="checkbox" className="peer me-1 accent-black dark:accent-white" id="strictMode" checked={strictMode} onChange={() => setStrictMode(!strictMode)} />
-          <label className="opacity-90 peer-checked:opacity-100" htmlFor="strictMode">
-            Strict mode
-          </label>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">If enabled, parse only formal date patterns.</p>
-        </div>
-
-        {/* 24-hour format */}
-        <div>
-          <input type="checkbox" className="peer me-1 accent-black dark:accent-white" id="24HourFormat" checked={use24HourFormat} onChange={() => setUse24HourFormat(!use24HourFormat)} />
-          <label className="opacity-90 peer-checked:opacity-100" htmlFor="24HourFormat">
-            Display 24-hour time format
-          </label>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">If enabled, display time using the 24-hour format for the timezones. Otherwise, use 12-hour format.</p>
-        </div>
-
-        {/* Sort timezones by time */}
-        <div>
-          <input type="checkbox" className="peer me-1 accent-black dark:accent-white" id="sortTimezonesByTime" checked={sortTimezonesByTime} onChange={() => setSortTimezonesByTime(!sortTimezonesByTime)} />
-          <label className="opacity-90 peer-checked:opacity-100" htmlFor="sortTimezonesByTime">
-            Sort timezones by time
-          </label>
-          <p className="text-sm text-neutral-600 dark:text-neutral-400">If enabled, sort list of timezones by their time difference. Otherwise, sort alphabetically.</p>
-        </div>
+        {settings.map(({ name, description, value, setValue }) => (
+          <div key={name}>
+            <input type="checkbox" className="peer me-1 accent-black dark:accent-white" id={name} checked={value} onChange={() => setValue(!value)} />
+            <label className="opacity-90 peer-checked:opacity-100" htmlFor={name}>
+              {name}
+            </label>
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">{description}</p>
+          </div>
+        ))}
       </div>
 
       <span>
