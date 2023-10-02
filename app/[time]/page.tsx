6cef0bf6436623494bcb4d6afab7fcd212245bca
Leon San José Larsson <leonlarsson8@gmail.com>
2023-10-02 18:01:39 +0200

    test 2

--- app/[time]/page.tsx
+++ app/[time]/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 export default ({ params: { time } }: { params: { time: string } }) => {
-  const newUrl = new URL("https://friendly-time.com/");
-  newUrl.searchParams.set("input", time);
-  return redirect(newUrl.toString());
+  useEffect(() => {
+    const newUrl = new URL("https://friendly-time.com/");
+    newUrl.searchParams.set("input", time);
+    return redirect(newUrl.toString());
+  });
 };
